13e85be301f7853c3f0024ba5b3c16ddf8b3669e
Rivalcoder <divi242006@gmail.com>
2025-03-02 20:33:54 +0530

    [Edit] Add Files

---
+++ src/pages/resume.js
@@ -0,0 +1,12 @@
+"use client"
+
+import { useState,useEffect } from "react";
+
+export default function Resumebuilder()
+{
+    return(
+        <>
+            <h1>Hello From Resume Builder </h1>
+        </>
+    )
+}
